83c3091713f2a9bf22b06dbefda9e6f3b37d858a
Konstantin0694 <78470517+Konstantin0694@users.noreply.github.com>
2021-03-01 14:52:16 +0100

    Perform_simulation_3

--- simulations.py
+++ simulations.py
@@ -13,31 +13,34 @@ from bandit_experiment import perform_experiments
 
 
 # =========================
-# old code, don't run!!!
+# Perform an experiment
 # =========================
 
+#Input parameters to perform experiment
+N_bandits=10
+N_experiments=1000
+N_episodes=500
+tau=0.5/N_bandits
+shock_prob=0
+tau_strategy="stable"
+
+(reward_m, tau_m, knowledge_m, exploration_m) =perform_experiments(N_bandits, N_experiments, N_episodes, tau, shock_prob, tau_strategy)
 
 # Calculate exploration probability
-exploration_prob= np.count_nonzero(exploration_matrix, axis=1)/N_episodes
+exploration_prob= np.count_nonzero(exploration_m, axis=1)/N_episodes
 exploration_percent_avg=np.mean(exploration_prob)
 
 # Calculate accumulated asset stock (rewards_cum)
-rewards_cum=np.cumsum(reward_history_matrix,axis=1)
+rewards_cum=np.cumsum(reward_m,axis=1)
 rewards_cum_avg=np.mean(rewards_cum,axis=0)
 rewards_end_avg=rewards_cum_avg[-1]
 
 # Calculate average knowledge at the end of the final period
-knowledge_avg_over_time=np.mean(knowledge_matrix, axis=0)
+knowledge_avg_over_time=np.mean(knowledge_m, axis=0)
 knowledge_end_avg=knowledge_avg_over_time[-1]
 
-# print(knowledge_matrix)
-# print(knowledge_cum)
-# knowledge_cum_avg=np.mean(knowledge_cum,axis=0)
-# print(knowledge_cum_avg)
-# knowledge_end_avg=knowledge_cum_avg[-1]
-# print(knowledge_end_avg)
 #Calculate average tau for each period
-tau_avg=np.mean(tau_matrix,axis=0)*N_bandits
+tau_avg=np.mean(tau_m,axis=0)*N_bandits
 tau_end_avg=tau_avg[-1]
 
 #Print main insights
